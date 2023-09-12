5d8a02086b4edc51c9c6267339eba32b1f04dbde
User <User@DESKTOP-4P5INAP>
2023-09-12 21:49:34 +0530

    MY PROJECT

---
+++ MyProject/src/main/java/mypack/B.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class B {
+
+		public void run()
+		{
+			System.out.println("Running");
+		}
+
+	}
+
+
+
---
+++ MyProject/src/main/java/mypack/F.java
@@ -0,0 +1,17 @@
+package mypack;
+
+
+	public class F extends B{
+		public void jump()
+		{
+			System.out.println("Animal");
+		}
+
+		public static void main(String[] args) {
+			F obj=new F();
+			obj.jump();
+			obj.run();		// TODO Auto-generated method stub
+
+		}
+
+	}
