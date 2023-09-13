b8073bf5264597c899841de5c911a8e15cd0c8b9
User <User@DESKTOP-4P5INAP>
2023-09-13 12:02:23 +0530

    String

---
+++ MyProject/src/main/java/stringpack/Forstring.java
@@ -0,0 +1,13 @@
+package stringpack;
+
+public class Forstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String s="Sneha S";
+ String s1= new String("Apple");
+ System.out.println(s);
+ System.out.println(s1);
+	}
+
+}
